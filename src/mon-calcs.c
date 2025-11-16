9cc37032343676e63760a7a297270e02eb7c6c1f
ajencis <shlubby01@gmail.com>
2025-11-16 14:32:21 -0500

    increase race skill growth

--- src/mon-calcs.c
+++ src/mon-calcs.c
@@ -198,7 +198,7 @@ struct scaling_data race_skill(const struct monster_race *mr, int which)
 
 	result.base = RND_TO_MULT((base_skill + 3 * SGN(base_skill)) / 4, 5);
 	result.r_xtra = base_skill - result.base;
-	result.p_xtra = MAX(0, ABS(base_skill) - mr->level * 4) / 2 * SGN(base_skill);
+	result.p_xtra = MAX(0, ABS(base_skill) - mr->level * 4) * SGN(base_skill);
 	result.p_xtra = RND_TO_MULT(result.p_xtra, 5);
 
 	return result;
