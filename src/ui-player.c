201847526119d63caa7226b4b4206ba1307e818a
ajencis <shlubby01@gmail.com>
2025-11-17 14:47:06 -0500

    fix bug with too many attacks in character sheet

--- src/ui-player.c
+++ src/ui-player.c
@@ -42,6 +42,7 @@
 #include "ui-input.h"
 #include "ui-object.h"
 #include "ui-output.h"
+#include "z-file.h"
 #include "z-form.h"
 #include "ui-player.h"
 
@@ -741,23 +742,33 @@ static struct panel *get_panel_midleft(void) {
 	return p;
 }
 
+static int combat_panel_min_size(struct player *p)
+{
+	struct attack *atk;
+	struct effect *ef;
+	int size = 6;
+
+	for (atk = p->mon.atk; atk; atk = atk->next) {
+		size++;
+		for (ef = atk->ef; ef; ef = ef->next) {
+			size++;
+		}
+	}
+
+	return size;
+}
+
 static struct panel *get_panel_combat(void) {
-	struct panel *p = panel_allocate(15);
+	struct panel *p;
 	int bth, dam;//, blws = 0;
 	struct py_attack_roll *aroll;
 	struct attack *atk;
-	//uint16_t i, j, k;
-	int hgt = 0;// cinfo_len;
-	//char name[32], title[32];
-
-	/*combat_info_t cinfo[] = {
-		combat_base_info,
-		death_touch_base_info
-	};
-
-	cinfo_len = N_ELEMENTS(cinfo);*/
+	int hgt = 0, panel_size;
 
 	update_mon_attacks(&player->mon);
+
+	panel_size = combat_panel_min_size(player);
+	p = panel_allocate(panel_size + 3);
 
 	/* AC */
 	panel_line(p, COLOUR_L_BLUE, "Armor", "[%d,%+d]",
@@ -774,13 +785,6 @@ static struct panel *get_panel_combat(void) {
 		char atk_title[80], range[80] = "";
 		int blows = atk->blows / 100, blow_frac = (atk->blows / 10) % 10, attr;
 		int num_choice = 0;
-
-		/*if (atk->obj) {
-			object_desc(atk_title, sizeof atk_title, atk->obj, ODESC_CAPITAL | ODESC_TERSE, player);
-		} else {
-			my_strcpy(atk_title, atk->title, sizeof atk_title);
-			//my_strcap_full(atk_title);
-		}*/
 
 		if (atk->range > 1) {
 			strnfmt(range, sizeof range, " (rng %i)", atk->range);
@@ -828,127 +832,6 @@ static struct panel *get_panel_combat(void) {
 			++hgt;
 		}
 	}
-
-	/*for (i = 0; i < player->mon.state.num_attacks; ++i) {
-		aroll = &player->mon.state.attacks[i];
-		bth = player->mon.state.skills[aroll->attack_skill] / BTH_PLUS_ADJ + aroll->to_hit;
-		bth = MAX(0, bth);
-		blws += aroll->blows;
-		uint16_t info_i;
-		int namelen, hitlen, infolen, linelen, spacelen;
-		bool one_line;
-		bool overwrite_prev = false; // if the previous info has no damage overwrite it with the next info
-
-		char info[5][80], proj_desc[8], hitstr[80];
-		int colours[5];
-
-		strnfmt(hitstr, sizeof hitstr, " %+d;", bth);
-
-		// grab the title
-		my_strcpy(name, aroll->name, sizeof name);
-		my_strcap_full(name);
-		strcat(name, ":");
-
-		info_i = 0;
-		for (j = 0; j < cinfo_len; ++j) {
-			int proj_id, dice, to_dam, sides;
-			int max_dam;
-
-			if (!cinfo[j](aroll, &dice, &sides, &to_dam, &proj_id)) {
-				continue;
-			}
-
-			if (overwrite_prev && info_i > 0) {
-				--info_i;
-			}
-
-			overwrite_prev = false;
-
-			struct projection *proj = &projections[proj_id];
-			colours[info_i] = proj->color;
-
-			int proj_desc_i;
-			int proj_len = strlen(proj->name);
-			for (k = 0, proj_desc_i = 0; k < proj_len && proj_desc_i < 3; ++k) {
-				if (!is_a_vowel(proj->name[k]) || k == 0) {
-					proj_desc[proj_desc_i] = proj->name[k];
-					++proj_desc_i;
-				}
-			}
-
-			// make a short description for the damage type
-			my_strcpy(proj_desc, proj->name, sizeof(proj));
-
-			// filter to just consonants
-			strfilter(proj_desc, sizeof(proj_desc), is_a_vowel);
-
-			// if it starts with a vowel we want to keep that vowel
-			if (is_a_vowel(proj->name[0])) {
-				char temp_str[80];
-				my_strcpy(temp_str, proj_desc, sizeof(temp_str));
-				strnfmt(proj_desc, sizeof(proj_desc), "%c%s", proj->name[0], temp_str);
-			}
-
-			// enshorten it
-			proj_desc[3] = '\0';
-
-			max_dam = MAX(0, dice * sides + to_dam);
-
-			// put the next damage string into the next info slot
-			if (max_dam <= 0) {
-				// we only want to show 0 damage if it's the only damage
-				// we skip it if we've already got lines and mark it to be overwritten if we have subsequent lines
-				if (j == 0) {
-					strnfmt(info[info_i], sizeof(info[info_i]), " 0 (%s)", proj_desc);
-					overwrite_prev = true;
-				}
-			} else if (!dice || !sides) {
-				strnfmt(info[info_i], sizeof(info[info_i]), " %d (%s)", to_dam, proj_desc);
-			} else if (sides == 1) {
-				strnfmt(info[info_i], sizeof(info[info_i]), " %d (%s)", to_dam + dice, proj_desc);
-			} else if (to_dam) {
-				strnfmt(info[info_i], sizeof(info[info_i]), " %dd%d%+d (%s)", dice, sides, to_dam, proj_desc);
-			} else {
-				strnfmt(info[info_i], sizeof(info[info_i]), " %dd%d (%s)", dice, sides, proj_desc);
-			}
-
-			++info_i;
-		}
-
-		namelen = strlen(name);
-		hitlen = strlen(hitstr);
-		infolen = strlen(info[0]); // +1 for semicolon
-		one_line = namelen + hitlen + infolen <= 21;
-		linelen = namelen + hitlen + (one_line ? infolen : 0);
-		spacelen = one_line ? 21 - linelen : 14 - linelen;
-		
-		strnfmt(title, sizeof title, "%s%*s%s", name, spacelen, "", hitstr);
-
-		for (j = 0; j < info_i; ++j) {
-			if (j > 0) {
-				panel_line(p, colours[j], " ", info[j]);
-				++hgt;
-			}
-			else if (one_line) {
-				panel_line(p, colours[j], title, info[j]);
-				++hgt;
-			}
-			else {
-				// name and info will be displayed on top of each other so cut them up
-				panel_line(p, colours[j], title, "");
-				panel_line(p, colours[j], " ", info[j]);
-				hgt += 2;
-			}
-		}
-	}
-
-	if (i > 0) {
-		panel_space(p);
-		blws /= MAX(1, player->mon.state.num_attacks);
-		panel_line(p, COLOUR_L_BLUE, "Blows", "%d.%d/turn",
-			blws / 100, (blws / 10) % 10);
-		hgt += 2;
-	}*/
 
 	/* Ranged */
 	aroll = &player->mon.state.ranged_attack;
